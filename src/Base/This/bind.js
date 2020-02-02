630ec2d7a7f236d9cc56a19bda457d0df6a9a2fd
ZephyrJS <zhengzefanyy@gmail.com>
2020-02-02 19:44:47 +0800

    软绑定代码实现

--- src/Base/This/bind.js
+++ src/Base/This/bind.js
@@ -1,5 +1,5 @@
 function selfBind(context) {
-    if(typeof this !== 'function') {
+    if (typeof this !== 'function') {
         throw new TypeError('Function prototype bind - what is trying to be bound is not callable!')
     }
     var self = this // 调用 bind 的 func
@@ -7,12 +7,26 @@ function selfBind(context) {
     var fBound = function () {
         var bindArgs = Array.prototype.slice.call(arguments)
         return self.apply(
-            this instanceof fBound ? this : context, 
+            this instanceof fBound ? this : context,
             args.concat(bindArgs)
         )
     }
-    var FNOP = function() {}
+    var FNOP = function () { }
     FNOP.prototype = this.prototype
     fBound.prototype = new FNOP()
     return fBound
+}
+
+// 软绑定，如果绑定丢失则指向给定的对象，否则使用默认绑定
+function softBind(Obj) {
+    const args = Array.prototype.slice.call(arguments, 1)
+    const fn = this
+    let bound = function () {
+        return fn.apply(
+            (!this || this === (window || undefined)) ? Obj : this,
+            args.concat.apply(args, arguments)
+        )
+    }
+    bound.prototype = Obj.create(fn.prototype)
+    return bound
 }

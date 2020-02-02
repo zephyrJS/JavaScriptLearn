9f6ef5b2f34e845540396796ca538ff73ae04d94
ZephyrJS <zhengzefanyy@gmail.com>
2020-02-02 10:58:10 +0800

    新增随机数,防止时间复杂度退化到 O^2

--- src/DataStructure/quickSort.js
+++ src/DataStructure/quickSort.js
@@ -1,6 +1,5 @@
 const assert = require('assert')
 
-// version 1
 function qSort(arr) {
     function _qSort(arr, l, r) {
         if (l >= r) return
@@ -13,6 +12,12 @@ function qSort(arr) {
     // 对 arr[l, r] 部分进行 partition 操作
     // 返回 p， 使得 arr[l...p-1] < arr[p], arr[p+1...r] > arr[p]
     function partition(arr, l, r) {
+        // 随机取 p，防止时间复杂度退化成 O(n^2)
+        let randIndex = Math.floor(Math.random() * (r - l + 1)) + l
+        if (l !== randIndex) {
+            [arr[l], arr[randIndex]] = [arr[randIndex], arr[l]]
+        }
+
         let v = arr[l]
         let j = l
         for (let i = l + 1; i <= r; i++) {
